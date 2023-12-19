e21c46697c1c35e69261b789cfd5bef56beee65c
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-19 16:09:10 -0600

    Day 19

--- 19/19.py
+++ 19/19.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 fname = '19.in'
 
 
@@ -60,12 +63,92 @@ def part1():
     print(res)
 
 
-
-
 def part2():
     with open(fname) as f:
         all_lines = f.read()
+    rules, _ = all_lines.split('\n\n')
+    rules = rules.split('\n')
+    logic = {}
+    for rule in rules:
+        rule = rule.strip('}').split('{')
+        name = rule[0]
+        rule = rule[1].split(',')
+        logic[name] = []
+        for r in rule:
+            if ':' not in r:
+                logic[name].append(r)
+                continue
+            target = r.split(':')[1]
+            letter = r[0]
+            compare = r[1]
+            value = int(r[2:].split(':')[0])
+            logic[name].append((letter, compare, value, target))
 
+
+    def evaluate(ranges):
+        res = 1
+        res *= ranges['x'][1] - ranges['x'][0] + 1
+        res *= ranges['m'][1] - ranges['m'][0] + 1
+        res *= ranges['a'][1] - ranges['a'][0] + 1
+        res *= ranges['s'][1] - ranges['s'][0] + 1
+        return res
+
+    res = 0
+    stack = []
+    initial_ranges = {
+        'x': [1, 4000],
+        'm': [1, 4000],
+        'a': [1, 4000],
+        's': [1, 4000],
+    }
+    stack.append(('in', initial_ranges))
+    while stack:
+        work, ranges = stack.pop()
+        bad = False
+        for letter in ranges:
+            if ranges[letter][1] < ranges[letter][0]:
+                bad = True
+                break
+        if bad:
+            continue
+
+        if work == 'A':
+            res += evaluate(ranges)
+            continue
+        elif work == 'R':
+            continue
+        else:
+            for i in range(len(logic[work])):
+                if i == len(logic[work]) - 1:
+                    stack.append((logic[work][i], ranges))
+                    break
+
+                letter, compare, value, target = logic[work][i]
+                if compare == '>':
+                    if ranges[letter][0] > value:
+                        stack.append((target, ranges))
+                        break
+                    elif ranges[letter][1] < value:
+                        continue
+                    else:
+                        new_ranges = deepcopy(ranges)
+                        new_ranges[letter][0] = value + 1
+                        stack.append((target, new_ranges))
+                        ranges[letter][1] = value
+                        continue
+                elif compare == '<':
+                    if ranges[letter][1] < value:
+                        stack.append((target, ranges))
+                        break
+                    elif ranges[letter][0] > value:
+                        continue
+                    else:
+                        new_ranges = deepcopy(ranges)
+                        new_ranges[letter][1] = value - 1
+                        stack.append((target, new_ranges))
+                        ranges[letter][0] = value
+                        continue
+    print(res)
 
 if __name__ == "__main__":
     part1()
